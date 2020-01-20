589c308f06926b617ca72c75c3041fffad580a6a
Adrian Sidor <adriansidor11@wp.pl>
2020-01-20 19:32:27 +0100

    Unit tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,12 +1,48 @@
+
+
 public class Calculator {
 
-    public int calculate(int a, int b) {
-        return a+b;
+    public int calculate(Integer num1, Integer num2) {
+
+        if (num1 == null || num2 == null) {
+            throw new IllegalArgumentException("Parametr nie może być nullem!");
+        }
+
+        return num1 + num2;
     }
 
-    public double calculateIncome(int grossIncome, int tax) {
-        double nettoIncome = grossIncome - grossIncome * tax/100;
+    public int substract(Integer num1, Integer num2) {
 
-        return nettoIncome;
+        if (num1 == null || num2 == null) {
+            throw new IllegalArgumentException("Parametr nie może być nullem!");
+        }
+
+        return num1 - num2;
     }
+
+    public int multiply(Integer num1, Integer num2) {
+
+        if (num1 == null || num2 == null) {
+            throw new IllegalArgumentException("Parametr nie może być nullem!");
+        }
+
+        return num1 * num2;
+    }
+
+    public int divide(Integer num1, Integer num2) {
+
+        if (num1 == null || num2 == null) {
+            throw new IllegalArgumentException("Parametr nie może być nullem!");
+        }
+
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Dzielnik nie może być zerem");
+        }
+
+        return num1 / num2;
+    }
+
+
+
+
 }
